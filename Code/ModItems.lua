ec0b8517fbe76a240d8310a6b3eef11e82b543fc
Nommy <199458351+nommygit@users.noreply.github.com>
2025-07-24 02:40:09 +0930

    Move SurvivorName functions to ModItems.lua

--- Code/ModItems.lua
+++ Code/ModItems.lua
@@ -1,3 +1,26 @@
+-- Returns a table array containing the full names of all defined survivors (CharacterDefs)
+local function FSaT_SurvivorNamesArray()
+	local names = {}
+	ForEachPreset("CharacterDef", function(unit, group) 
+		table.insert(names, _InternalTranslate(unit.FirstName).." ".._InternalTranslate(unit.LastName))
+	end)
+	return names
+end
+
+-- Returns a table which is tbl with survivor names added to the end
+local function FSaT_AppendSurvivorNamesArray(tbl)
+	local survivorNames = FSaT_SurvivorNamesArray() or {}
+	for _, name in ipairs(survivorNames) do
+		table.insert(tbl, name)
+	end
+	return tbl
+end
+
+-- 
+function FSaT_OptionApplyToChoiceList()
+	return FSaT_AppendSurvivorNamesArray({"*All Survivors*"})
+end
+
 --[[
 Substitute(template, values)
 
