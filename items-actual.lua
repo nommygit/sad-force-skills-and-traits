e46afdb805282e40d65a7cd22e73cd6255021f3f
Nommy <199458351+nommygit@users.noreply.github.com>
2025-07-24 08:36:16 +0930

    Improve documentation about items.lua and it's backup items-actual.lua

--- items-actual.lua
+++ items-actual.lua
@@ -4,9 +4,9 @@
 	Fix by adding the line below to item.lua (it shoudld be the only thing in the file).
 
 	To Edit/Add mod items: 
-		1. Change files in Templates folder (can copy mod editor generated code to here)
-		2. Run 'python templates_generator.py' to rebuild Templates.lua
-			FSaT_BuildItems() will process Templates.lua contents to those mod items
+		1. Change files in Templates folder (can copy mod editor generated code to there)
+		2. Run 'python3 templates_generator.py' to rebuild Templates.lua
+			FSaT_BuildItems() will convert Templates.lua contents to equivalent mod items at runtime
 	
 	This file mearly serves as a backup for items.lua, it's not used otherwise.
 
--- items.lua
+++ items.lua
@@ -4,8 +4,8 @@
 	Fix by adding the line below to item.lua (it shoudld be the only thing in the file).
 
 	To Edit/Add mod items: 
-		1. Change files in Templates folder (can copy mod editor generated code to here)
-		2. Run 'python templates_generator.py' to rebuild Templates.lua
-			FSaT_BuildItems() will process Templates.lua contents to those mod items
+		1. Change files in Templates folder (can copy mod editor generated code to there)
+		2. Run 'python3 templates_generator.py' to rebuild Templates.lua
+			FSaT_BuildItems() will convert Templates.lua contents to equivalent mod items at runtime
 ]]
 return FSaT_BuildItems()
